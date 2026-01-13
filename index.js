a0a49d3120de49127f388191dc9470cacd68e5eb
shajid027 <shajidhasan013@gmail.com>
2026-01-13 15:19:45 +0600

    initial  commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const authRouter = require('./routers/auth.route.js');
+const movieRouter = require('./routers/movie.route.js');
+const connectDB = require('./utils/db.js');
+const errorMiddleware = require('./middlewares/error.middleware.js');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/auth', authRouter);
+app.use('/api/movies', movieRouter);
+
+app.use(errorMiddleware);
+
+const PORT = process.env.PORT || 8000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
